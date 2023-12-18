0fcb4721937634f35c87f79d795c4e04034f39ad
murderteeth <murdertxxth@gmail.com>
2023-12-18 23:25:17 +0000

    Remove mock subject

--- hooks/useSubjects.tsx
+++ hooks/useSubjects.tsx
@@ -17,7 +17,7 @@ export const context = createContext<Context>({} as Context)
 export const useSubjects = () => useContext(context)
 
 export default function SubjectsProvider({ children }: { children: ReactNode }) {
-  const [subjects, setSubjects] = useState<Subject[]>([{ type: 'vault', id: '0xA013Fbd4b711f9ded6fB09C1c0d358E2FbC2EAA0'}])
+  const [subjects, setSubjects] = useState<Subject[]>([])
 	return <context.Provider value={{ subjects, setSubjects }}>
 		{children}
 	</context.Provider>
