23f17be2347680b310b843b529cd22e675700137
murderteeth <murdertxxth@gmail.com>
2023-12-19 00:23:02 +0000

    Increase nextjs function timeout to 60s

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,3 +1,5 @@
+export const maxDuration = 60
+
 import { NextRequest, NextResponse } from 'next/server'
 import { ChatCompletionMessageParam } from 'openai/resources/index.mjs'
 import { sleep } from 'openai/core.mjs'
